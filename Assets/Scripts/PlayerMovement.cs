8589c9dda695b732c5a28c65e51097ea50f91036
SweetieWind <100509108+SweetieWind@users.noreply.github.com>
2024-08-20 05:09:38 -0600

    Animator and few animations to player added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,11 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D RB2D;
     public SpriteRenderer sprite;
     public float moveSpeed;
+    public Animator animator;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         playerMovement();
@@ -25,15 +24,63 @@ public class PlayerMovement : MonoBehaviour
         Vector2 velocity =  new Vector2 (horizontalInput * moveSpeed, verticalInput * moveSpeed);
         RB2D.velocity = velocity;
         flipSprite(horizontalInput);
+        spriteUpAndDown(verticalInput);
+        playerWalking(horizontalInput, verticalInput);
     }
     private void flipSprite(float horizontal)
     {
         if (horizontal < 0) {
             sprite.flipX = true;
+            isLookingRight();
         }
         else if (horizontal > 0)
         {
             sprite.flipX = false;
+            isLookingRight();
         }
+    }
+    void spriteUpAndDown(float vertical)
+    {
+        if (vertical< 0)
+        {
+            isLookingFront();
+        }
+        else if (vertical > 0)
+        {
+            isLookingUp();
+        }
+    }
+    void playerWalking(float horizontal, float vertical)
+    {
+        if(horizontal == 0 && vertical== 0)
+        {
+            stopWalking();
+        }
+        else
+        {
+            isWalking();
+        }
+    }
+    void isLookingRight()
+    {
+        animator.SetTrigger("seeingSide");
+    }
+    void isLookingUp()
+    {
+        animator.SetTrigger("seingTop");
+
+    }
+    void isLookingFront()
+    {
+        animator.SetTrigger("seingFront");
+
+    }
+    void isWalking()
+    {
+        animator.SetBool("isWalking", true);
+    }
+    void stopWalking()
+    {
+        animator.SetBool("IsWalking", false);
     }
 }
